cf99c9491e8eb39f3a3987f5b7f7eea52ec3212f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-24 15:10:59 -0700

    Add default for args

--- src/app/args.rs
+++ src/app/args.rs
@@ -4,14 +4,33 @@
 //! We have these args in their own file in order to be flexible,
 //! such as being able to start our app with other arg parsers.
 
-use std::default::Default;
-use std::path::PathBuf;
+use ::std::path::PathBuf;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Args {
-    pub(crate) output_path: Option<PathBuf>,
+    pub(crate) input_file_path: Option<PathBuf>,
+    pub(crate) input_directory_path: Option<PathBuf>,
+    pub(crate) input_extension: Option<String>,
+    pub(crate) output_file_path: Option<PathBuf>,
+    pub(crate) output_directory_path: Option<PathBuf>,
+    pub(crate) output_extension: Option<String>,
     pub(crate) paths: Option<Vec<PathBuf>>,
-    pub(crate) template_file: String,
-    pub(crate) templates_glob: String,
+    pub(crate) template_name: Option<String>,
+    pub(crate) template_glob: Option<String>,
     pub(crate) verbose: u8,
+}
+
+impl ::std::default::Default for Args {
+    fn default() -> Self { Self {
+        input_file_path: None,
+        input_directory_path: None,
+        input_extension: None,
+        output_file_path: None,
+        output_directory_path: None,
+        output_extension: None,
+        paths: None,
+        template_name: None,
+        template_glob: None,
+        verbose: 0,
+    } }
 }
