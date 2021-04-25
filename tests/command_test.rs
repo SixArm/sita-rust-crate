14bb4ff27f75e394c327a8351a17fd44e760c576
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-24 17:18:20 -0700

    Drop manual test files

--- tests/command_test.rs
+++ tests/command_test.rs
@@ -1,6 +1,6 @@
 use ::indoc::indoc;
 use ::std::process::Command;
-use ::std::path::Path;
+use ::std::path::PathBuf;
 
 const SITA: &str = "./target/debug/sita";
 
@@ -41,63 +41,78 @@ const FAB_OUTPUT_HTML: &str = indoc!{r#"
 
 "#};
 
-fn fab_alpha() {
-    ::std::fs::write("tmp/alpha.md", FAB_INPUT_MARKDOWN).expect("write");
+fn fab_path(s: &str) -> PathBuf {
+    [env!("CARGO_MANIFEST_DIR"), "tmp", s].iter().collect::<PathBuf>()
 }
 
-fn fab_template() {
-    ::std::fs::write("tmp/template.html", FAB_TERA_TEMPLATE).expect("write");
+fn fab_input_file(path: &PathBuf) {
+    ::std::fs::write(path, FAB_INPUT_MARKDOWN).expect("write");
+}
+
+fn fab_template_file(path: &PathBuf) {
+    ::std::fs::write(path, FAB_TERA_TEMPLATE).expect("write");
+}
+
+
+#[test]
+fn test_command_x_default() {
+    // Prep
+    let input_path = fab_path("alpha.md");
+    fab_input_file(&input_path);
+    let output_path = fab_path("alpha.html");
+    if output_path.exists() {
+        ::std::fs::remove_file(&output_path).expect("prep");
+    }
+    // Run
+    assert!(!&output_path.exists());
+    let _output = Command::new(SITA)
+        .arg(&input_path)
+        .output()
+        .expect("failure");
+    assert!(&output_path.exists());
+    assert_eq!(::std::fs::read_to_string(output_path).expect("output"), FAB_OUTPUT_HTML);
 }
 
 #[test]
-fn test_1() {
-    fab_alpha();
-    let f = "tmp/alpha.html";
-    if Path::new(f).exists() {
-        ::std::fs::remove_file(f).expect("before");
+fn test_command_x_output_file() {
+    // Prep
+    let input_path = fab_path("alpha.md");
+    fab_input_file(&input_path);
+    let output_path = fab_path("alpha.html");
+    if output_path.exists() {
+        ::std::fs::remove_file(&output_path).expect("prep");
     }
-    assert!(!Path::new(f).exists());
+    // Run
+    assert!(!output_path.exists());
     let _output = Command::new(SITA)
-        .arg("tmp/alpha.md")
+        .arg(&input_path)
+        .arg("--output-file")
+        .arg(&output_path)
         .output()
         .expect("failure");
-    assert!(Path::new(f).exists());
-    assert_eq!(::std::fs::read_to_string(f).expect("output"), FAB_OUTPUT_HTML);
+    assert!(output_path.exists());
+    assert_eq!(::std::fs::read_to_string(output_path).expect("output"), FAB_OUTPUT_HTML);
 }
 
 #[test]
-fn test_arg_output() {
-    fab_alpha();
-    let f = "tmp/bravo.html";
-    if Path::new(f).exists() {
-        ::std::fs::remove_file(f).expect("before");
+fn test_command_x_template_name() {
+    // Prep
+    let input_path = fab_path("alpha.md");
+    fab_input_file(&input_path);
+    let template_path = fab_path("template.html");
+    fab_template_file(&template_path);
+    let output_path = fab_path("alpha.html");
+    if output_path.exists() {
+        ::std::fs::remove_file(&output_path).expect("before");
     }
-    assert!(!Path::new(f).exists());
+    // Run
+    assert!(!output_path.exists());
     let _output = Command::new(SITA)
-        .arg("tmp/alpha.md")
-        .arg("--output")
-        .arg("tmp/bravo.html")
+        .arg(&input_path)
+        .arg("--template-name")
+        .arg(&template_path)
         .output()
         .expect("failure");
-    assert!(Path::new(f).exists());
-    assert_eq!(::std::fs::read_to_string(f).expect("output"), FAB_OUTPUT_HTML);
-}
-
-#[test]
-fn test_arg_template() {
-    fab_alpha();
-    fab_template();
-    let f = "tmp/alpha.html";
-    if Path::new(f).exists() {
-        ::std::fs::remove_file(f).expect("before");
-    }
-    assert!(!Path::new(f).exists());
-    let _output = Command::new(SITA)
-        .arg("tmp/alpha.md")
-        .arg("--template")
-        .arg("tmp/template.html")
-        .output()
-        .expect("failure");
-    assert!(Path::new(f).exists());
-    assert_eq!(::std::fs::read_to_string(f).expect("output"), FAB_OUTPUT_HTML);
+    assert!(output_path.exists());
+    assert_eq!(::std::fs::read_to_string(output_path).expect("output"), FAB_OUTPUT_HTML);
 }
