e557085cce21ca9d0ec163561576362e9f19a119
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-06-11 13:20:51 -0700

    Add types

---
+++ src/types.rs
@@ -0,0 +1,17 @@
+#[allow(dead_code)] pub type HtmlStr = str;
+#[allow(dead_code)] pub type HtmlString = String;
+
+#[allow(dead_code)] pub type JsonStr = str;
+#[allow(dead_code)] pub type JsonString = String;
+
+#[allow(dead_code)] pub type MarkdownStr = str;
+#[allow(dead_code)] pub type MarkdownString = String;
+
+#[allow(dead_code)] pub type TomlStr = str;
+#[allow(dead_code)] pub type TomlString = String;
+
+#[allow(dead_code)] pub type UrlStr = str;
+#[allow(dead_code)] pub type UrlString = String;
+
+#[allow(dead_code)] pub type YamlStr = str;
+#[allow(dead_code)] pub type YamlString = String;
