a32848f95be6d05475cfc0f4fbc2f3725254a40c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-06-20 15:40:00 -0700

    Add glob type

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,6 @@
+#[allow(dead_code)] pub type GlobStr = str;
+#[allow(dead_code)] pub type GlobString = String;
+
 #[allow(dead_code)] pub type HtmlStr = str;
 #[allow(dead_code)] pub type HtmlString = String;
 
