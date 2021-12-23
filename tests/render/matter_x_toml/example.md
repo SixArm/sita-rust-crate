decfd387a412493fac0482ef565f80e18e648210
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-12-23 21:11:13 +0100

    Add system tests for matter with HTML, JSON, TOML, YAML

---
+++ tests/render/matter_x_toml/example.md
@@ -0,0 +1,5 @@
++++
+title = "Title Example"
++++
+# alpha
+bravo
---
+++ tests/render_test.rs
@@ -0,0 +1,39 @@
+#[path = "util.rs"]
+mod util;
+use util::*;
+
+#[test]
+fn test_minimal() {
+    test_with_base_path_and_default_input_template_actual_expect(
+        TESTS_DIR.join("render").join("minimal")
+    );
+}
+
+#[test]
+fn test_matter_x_html() {
+    test_with_base_path_and_default_input_template_actual_expect(
+        TESTS_DIR.join("render").join("matter_x_html")
+    );
+}
+
+#[test]
+fn test_matter_x_json() {
+    test_with_base_path_and_default_input_template_actual_expect(
+        TESTS_DIR.join("render").join("matter_x_json")
+    );
+}
+
+#[test]
+fn test_matter_x_toml() {
+    test_with_base_path_and_default_input_template_actual_expect(
+        TESTS_DIR.join("render").join("matter_x_toml")
+    );
+}
+
+#[test]
+fn test_matter_x_yaml() {
+    test_with_base_path_and_default_input_template_actual_expect(
+        TESTS_DIR.join("render").join("matter_x_yaml")
+    );
+}
+
